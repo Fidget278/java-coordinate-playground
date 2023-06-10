0bfb47769244a8320b644fa659d6813be6f0ff68
fidget278 <mills_club@naver.com>
2023-06-11 00:54:49 +0900

    피드백 수정

--- src/main/java/domain/Points.java
+++ src/main/java/domain/Points.java
@@ -1,9 +1,26 @@
 package domain;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class Points implements Area {
+
+    private static final int NUM_OF_VERTICES_OF_LINE = 2;
+    private static final int NUM_OF_VERTICES_OF_TRIANGLE = 3;
+    private static final int NUM_OF_VERTICES_OF_RECTANGLE = 4;
+    private static final Map<Integer, Function<Point[], Points>> classifier = new HashMap<>();
+
     private Point[] points;
+
+    static {
+        classifier.put(NUM_OF_VERTICES_OF_LINE, Line::new);
+        classifier.put(NUM_OF_VERTICES_OF_TRIANGLE, Triangle::new);
+        classifier.put(NUM_OF_VERTICES_OF_RECTANGLE, Rectangle::new);
+    }
+
 
     public Points(Point[] points) {
         this.points = points;
@@ -26,13 +43,7 @@ public class Points implements Area {
         if(points.length == 0 || points.length > 4)
             throw new IllegalArgumentException("선, 삼각형, 사각형만 생성 가능합니다. 알맞은 갯수의 좌표를 입력하세요.");
 
-        if(points.length > 3)
-            return new Rectangle(points);
-
-        if(points.length > 2)
-            return new Triangle(points);
-
-        return new Line(points);
+        return classifier.get(points.length).apply(points);
     }
 
     @Override
